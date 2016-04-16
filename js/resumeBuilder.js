8e7db4ee5ccc09b26814457e631b4a39c6390ad4
Mike Mitchell <mmitchell@weber.edu>
2016-04-16 12:43:42 -0600

    Appending info to the header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,8 @@
-$("#main").append("Mike Mitchell");
+var name = "Mike Mitchell";
+var formattedName = HTMLheaderName.replace(/%data%/i, name);
+
+var role = "Front End Developer";
+var formattedRole = HTMLheaderRole.replace(/%data%/i, role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
