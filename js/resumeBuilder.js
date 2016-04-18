3a8eb81235411121583e9aa66ee7bcc98bc3efb9
Mike Mitchell <mmitchell@weber.edu>
2016-04-18 17:47:29 -0600

    Creating objects - wheeeee

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,25 @@
 var name = "Mike Mitchell";
 var formattedName = HTMLheaderName.replace(/%data%/i, name);
-
 var role = "Front End Developer";
 var formattedRole = HTMLheaderRole.replace(/%data%/i, role);
+var bio = {
+	"name" : "Mike Mitchell",
+	"role" : "Front End Developer",
+	"contacts" : { 
+		"email" : "mmitchell@weber.edu",
+		"github" : "hummusatune",
+		"mobile" : "801-389-5553",
+		"location" : "Utah"
+	},
+	"pic" : "images/fry.jpg",
+	"welcome" : "Hello! I'm Mike.",
+	"skills" : [
+		"HTML", "CSS", "JavaScript", "stuff", "things"
+	]
+}
+var workHistory = {}
+var education = {}
+
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
