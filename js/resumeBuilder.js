edf11393e6abb771bf3a114b445981d53ed4a0bb
Mike Mitchell <mmitchell@weber.edu>
2016-04-19 17:45:16 -0600

    Cleaned up resume builder and added work, bio, projects, education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,3 @@
-var name = "Mike Mitchell";
-var formattedName = HTMLheaderName.replace(/%data%/i, name);
-var role = "Front End Developer";
-var formattedRole = HTMLheaderRole.replace(/%data%/i, role);
 var bio = {
 	"name" : "Mike Mitchell",
 	"role" : "Front End Developer",
@@ -17,9 +13,79 @@ var bio = {
 		"HTML", "CSS", "JavaScript", "stuff", "things"
 	]
 }
-var workHistory = {}
-var education = {}
 
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Weber State University",
+			"title" : "eLearning System Administrator",
+			"dates" : "2014-present",
+			"description" : "Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter."
+		},
+		{
+			"employer" : "Self Employed",
+			"title" : "Web Designer",
+			"dates" : "2013-present",
+			"description" : "Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter."
+		},
+		{
+			"employer" : "Weber State University",
+			"title" : "Instructional Designer",
+			"dates" : "2012-2013",
+			"description" : "Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter."
+		}
+	]
+}
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+var education = {
+	"schools" : [
+		{
+			"name" : "Utah State University",
+			"city" : "Logan, Utah",
+			"degree" : "M.Ed.",
+			"major" : "Instructional Design",
+			"graduated" : "2011",
+			"url" : "https://www.usu.edu/"
+		},
+		{
+			"name" : "Weber State University",
+			"city" : "Ogden, Utah",
+			"degree" : "BA",
+			"major" : "English",
+			"graduated" : "2007",
+			"url" : "https://weber.edu"
+		}
+	]
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "Some sort of project",
+			"date" : "January 2016",
+			"description" : "Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of silver dagger, releases juice better than cutting. Full moon Whomping Willow three turns should do it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, he will rise again and he will come for us, headmaster Erumpent horn. Fenrir Grayback horseless carriages ‘zis is a chance many would die for!",
+			"images" : [
+				"",
+				""
+			]
+		},
+		{
+			"title" : "Another sort of project",
+			"date" : "November 2016",
+			"description" : "Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of silver dagger, releases juice better than cutting. Full moon Whomping Willow three turns should do it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, he will rise again and he will come for us, headmaster Erumpent horn. Fenrir Grayback horseless carriages ‘zis is a chance many would die for!",
+			"images" : [
+				"",
+				""
+			]
+		},
+		{
+			"title" : "This kind of project",
+			"date" : "May 2016",
+			"description" : "Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of silver dagger, releases juice better than cutting. Full moon Whomping Willow three turns should do it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, he will rise again and he will come for us, headmaster Erumpent horn. Fenrir Grayback horseless carriages ‘zis is a chance many would die for!",
+			"images" : [
+				"",
+				""
+			]
+		}
+	]
+}
